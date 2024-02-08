2848d5c9bd53ea225e243b3c99b4365574867c34
Robin Green <robin@designingreen.co.uk>
2024-02-08 14:44:51 +0000

    clean load on gmap

--- header.php
+++ header.php
@@ -42,12 +42,25 @@
         <script src="https://kit.fontawesome.com/d256b19c43.js" crossorigin="anonymous"></script>
 
 		<!-- The callback parameter is required, so we use console.debug as a noop -->
+		<!--
     	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAC23mG6_0iLjkGf8GMpVfz3sF3OFzodgc&callback=initMap" async defer></script>
+    -->
+
+    	<script>
+		  (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+		    key: "AIzaSyAC23mG6_0iLjkGf8GMpVfz3sF3OFzodgc",
+		    v: "weekly",
+		    // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+		    // Add other bootstrap parameters as needed, using camel case.
+		  });
+		</script>
 
     	<script>
 		let map;
-		function initMap() {
-			map = new google.maps.Map(document.getElementById('map'), {
+		async function initMap() {
+  			const { Map } = await google.maps.importLibrary("maps");
+
+			map = new Map(document.getElementById("map"), {
 				center: { lat: 50.87752255, lng: -0.15155365 },
 				zoom: 12,
 			});
@@ -66,6 +79,7 @@
 				title: 'Queens Park Tennis Club',
 			});
 		}
+		initMap();
     	</script>
 
 		<!-- Google tag (gtag.js) -->
