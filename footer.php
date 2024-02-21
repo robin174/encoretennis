aac7c3d0bc0125fa989c7710ea3e49c8b85f69aa
Robin Green <robin@designingreen.co.uk>
2024-02-21 16:01:33 +0000

    Rejig footer

--- footer.php
+++ footer.php
@@ -47,18 +47,19 @@
                	</section>
                 <section class="footer-layer02">
 	                <div class="row">
-						<div class="col-6">
-	                		<p class="atm-copyright">&copy;&nbsp;<?php echo date('Y'); ?>&nbsp;<?php the_field('et_company', 'option'); ?><br>
-	       					All rights reserved.</p>
-	       					<p class="atm-copyright"><a href="https://indd.adobe.com/view/1e7bd97a-2e9d-40a4-bcca-09ba3b120a8e" target="_blank" title="Caba Consultancy">Website designed by Caba Consultancy</a></p>
-	                	</div>
-	                	<div class="col-6">
+	                	<div class="col-12 col-md-6 order-md-2">
 							<div class="mol__unit-mailchimp">
 								<div class="row g-0">
 									<?php get_template_part('template-parts/unit-newsletter'); ?>
 								</div>
 							</div>
 	                	</div>
+						<div class="col-12 col-md-6 order-md-1">
+	                		<p class="atm-copyright">&copy;&nbsp;<?php echo date('Y'); ?>&nbsp;<?php the_field('et_company', 'option'); ?><br>
+	       					All rights reserved.</p>
+	       					<p class="atm-copyright"><a href="https://indd.adobe.com/view/1e7bd97a-2e9d-40a4-bcca-09ba3b120a8e" target="_blank" title="Caba Consultancy">Website designed by Caba Consultancy</a></p>
+	                	</div>
+	                	
 	                </div>
                 </section>
 
