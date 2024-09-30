69fdfdc4172dd1431cd4f5f786f9a138382599e4
Robin Green <robin@designingreen.co.uk>
2024-09-30 14:46:56 +0100

    update to template, new location

---
+++ template-parts/unit-teamup-stjohns.php
@@ -0,0 +1,16 @@
+<div style="padding: 30px 20px; background: white;">
+	<script src='https://teamupstatic.com/teamup.js' type='text/javascript'></script>
+	<script type='text/javascript'>
+		teamup.calendar({
+			provider_id:7767495,
+			type:"main",
+			width:"100%",
+			specific_pages:[],
+			view:"schedule.month",
+			venue_ids:[57305],
+			free_trial_membership:"",
+			disable_venue_selector:true,
+			version: 3
+		});
+	</script>
+</div>
